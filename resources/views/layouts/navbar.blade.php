2554d641175a6b14a37fbc4311871e9d66e3a476
Joaquin-Julian <jjuliangalvez03@gmail.com>
2025-02-09 17:47:21 +0100

    estilo del header acabado

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,25 +1,54 @@
-<div x-data="{ open: false }" 
+<div x-data="{ open: false, abierto: false }" 
     :class="open ? 'h-full' : 'h-auto'"
+    @mouseenter="abierto = true" 
+    @mouseleave="abierto = false"
     class="fixed w-full bg-custom-black text-white 
     inset-x-0 top-0 sm:w-[100px] sm:flex sm:flex-col sm:align-center
     sm:inset-y-0 sm:left-0 transition-all duration-300 sm:hover:w-[400px]">
     
-    <!-- Botón de Toggle SOLO para móviles -->
     <div class="flex justify-end">
         <img @click="open = !open" class="w-20 h-15 mt-3 mb-3 mr-3 cursor-pointer" 
              src="{{ asset('images/rayas_navegacion.png') }}" 
              alt="navegacion">
     </div>
 
-    <!-- Menú desplegable -->
-    <div :class="open ? 'flex flex-col' : 'hidden'"
-         class="sm:flex sm:flex-col display-column justify-between h-3/5 mt-20">
-        <div><img class="w-15 h-10 mt-3 mx-auto mb-3" src="{{ asset('images/instrumentos/guitarra.png') }}"></div>
-        <div><img class="w-15 h-10 mt-3 mx-auto mb-3" src="{{ asset('images/instrumentos/guitarra_española.png') }}"></div>
-        <div><img class="w-15 h-10 mt-3 mx-auto mb-3" src="{{ asset('images/instrumentos/bateria.png') }}"></div>
-        <div><img class="w-15 h-10 mt-3 mx-auto mb-3" src="{{ asset('images/instrumentos/teclado.png') }}"></div>
-        <div><img class="w-15 h-10 mt-3 mx-auto mb-3" src="{{ asset('images/instrumentos/piano.png') }}"></div>
-        <div><img class="w-15 h-10 mt-3 mx-auto mb-3" src="{{ asset('images/instrumentos/mesa_mezcla.png') }}"></div>
+    <div :class="open ? 'flex flex-col items-center' : 'hidden'"
+        class="sm:flex sm:flex-col sm:items-center display-column justify-between h-3/5 mt-20">
+        <div class="flex items-center justify-center gap-4 relative group hover:cursor-pointer">
+            <p :class="{'block' : open || abierto, 'hidden' : !(open || abierto)}" class="text-xl whitespace-nowrap overflow-hidden">Guitarras electricas</p>
+            <img class="w-15 h-10 mt-3 mb-3" src="{{ asset('images/instrumentos/guitarra.png') }}">
+            <div class="absolute inset-x-0 bottom-0 border-b-2 border-white scale-x-0 group-hover:scale-x-100 transition-transform origin-center"></div>
+        </div>
+        <div class="flex items-center justify-center gap-4 relative group hover:cursor-pointer">
+            <p :class="{'block' : open || abierto, 'hidden' : !(open || abierto)}" class="text-xl whitespace-nowrap overflow-hidden">Guitarras españolas</p>
+            <img class="w-15 h-10 mt-3 mb-3" src="{{ asset('images/instrumentos/guitarra_española.png') }}">
+            <div class="absolute inset-x-0 bottom-0 border-b-2 border-white scale-x-0 group-hover:scale-x-100 transition-transform origin-center"></div>
+        </div>
+        <div class="flex items-center justify-center gap-4 relative group hover:cursor-pointer">
+            <p :class="{'block' : open || abierto, 'hidden' : !(open || abierto)}" class="text-xl whitespace-nowrap overflow-hidden">Baterias</p>
+            <img class="w-15 h-10 mt-3 mb-3" src="{{ asset('images/instrumentos/bateria.png') }}">
+            <div class="absolute inset-x-0 bottom-0 border-b-2 border-white scale-x-0 group-hover:scale-x-100 transition-transform origin-center"></div>
+        </div>
+        <div class="flex items-center justify-center gap-4 relative group hover:cursor-pointer">
+            <p :class="{'block' : open || abierto, 'hidden' : !(open || abierto)}" class="text-xl whitespace-nowrap overflow-hidden">Teclados</p>
+            <img class="w-15 h-10 mt-3 mb-3" src="{{ asset('images/instrumentos/teclado.png') }}">
+            <div class="absolute inset-x-0 bottom-0 border-b-2 border-white scale-x-0 group-hover:scale-x-100 transition-transform origin-center"></div>
+        </div>
+        <div class="flex items-center justify-center gap-4 relative group hover:cursor-pointer">
+            <p :class="{'block' : open || abierto, 'hidden' : !(open || abierto)}" class="text-xl whitespace-nowrap overflow-hidden">Pianos</p>
+            <img class="w-15 h-10 mt-3 mb-3" src="{{ asset('images/instrumentos/piano.png') }}">
+            <div class="absolute inset-x-0 bottom-0 border-b-2 border-white scale-x-0 group-hover:scale-x-100 transition-transform origin-center"></div>
+        </div>
+        <div class="flex items-center justify-center gap-4 relative group hover:cursor-pointer">
+            <p :class="{'block' : open || abierto, 'hidden' : !(open || abierto)}" class="text-xl whitespace-nowrap overflow-hidden">Mesas de mezcla</p>
+            <img class="w-15 h-10 mt-3 mb-3" src="{{ asset('images/instrumentos/mesa_mezcla.png') }}">
+            <div class="absolute inset-x-0 bottom-0 border-b-2 border-white scale-x-0 group-hover:scale-x-100 transition-transform origin-center"></div>
+        </div>
+        <div class="mt-16" :class="abierto ? 'block' : 'hidden'">
+            <div class="relative group hover:cursor-pointer">
+                <a class="text-xl mt-3" href="">Ver todo</a>
+                <div class="absolute inset-x-0 bottom-0 border-b-2 border-white scale-x-0 group-hover:scale-x-100 transition-transform origin-center"></div>
+            </div>
+        </div>
     </div>
-
 </div>
