5ab694a24c95ac8654417e50e4d640d9320b371e
JoaquimJulian <joaquim.julian@inslapineda.cat>
2025-03-27 13:15:21 +0100

    chatbot

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,5 +36,21 @@
     <x-popup-login />
     <x-popup-registro />
     <x-popup-anadirTarjeta />
+
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/67e52f4a4a9eca190b85dffa/1inbk1f97';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
 </body>
 </html>
